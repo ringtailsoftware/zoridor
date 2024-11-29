4aaed6b70725d012446dab607368a730b72da077
Toby Jaffey <toby@ringtailsoftware.co.uk>
2024-11-29 19:20:12 +0000

    Fix tests for new API

--- build.zig
+++ build.zig
@@ -53,6 +53,7 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         });
         exe_unit_tests.root_module.addImport("mibu", mibu.module("mibu"));
+        exe_unit_tests.root_module.addOptions("buildopts", options);
 
         const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
